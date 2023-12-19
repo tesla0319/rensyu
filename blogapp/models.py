c7a114089abcd40c5a83ed63182cba47a5e56611
TSUJI <yukika_tsuji@totec.co.jp>
2023-12-19 15:21:22 +0900

    モデル作成 改

--- blogapp/models.py
+++ blogapp/models.py
@@ -6,8 +6,8 @@ class BlogPost(models.Model):
     #カテゴリに設定する項目を入れ子のタプルとして定義
     #タプルの第1要素はモデルが使用する値、
     #第2要素は管理サイトの選択メニューに表示する文字列
-    CATEGORY = (('science','科学のこと')
-                ('dailylife','日常のこと')
+    CATEGORY = (('science','科学のこと'),
+                ('dailylife','日常のこと'),
                 ('music','音楽のこと'))
 
 #タイトル用のフィールド
