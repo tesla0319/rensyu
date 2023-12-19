c084e24a87b3c7fae48b5001c67916f5f4716907
TSUJI <yukika_tsuji@totec.co.jp>
2023-12-19 11:18:19 +0900

    ルーティング設定（blogapp）

---
+++ blogapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+#URLconfのURLパターンを逆引きできるようにアプリ名を登録
+app_name = 'blogapp'
+
+#URLパターンを登録するためのリスト
+urlpatterns = [
+    #http(s)://ホスト名/以下のパスが''（無し）の場合
+    #viewsモジュールのIndexViewを実行
+    #URLパターン名は'index'
+    path('',views.indexView.as_view(),name='index'),
+]
